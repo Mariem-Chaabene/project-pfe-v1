f05217171fc58a643ecb7d4c29ce5b0915390e82
Mariem Chaabene <chabenemariem@gmail.com>
2022-03-30 13:37:08 +0100

    last

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import {AgmCoreModule} from '@agm/core';
+import { DBServices } from './db.services';
 
 
 @NgModule({
@@ -47,7 +48,9 @@ import {AgmCoreModule} from '@agm/core';
     })
     
   ],
-  providers: [],
+  providers: [
+    DBServices
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/stade/stade.component.ts
+++ src/app/stade/stade.component.ts
@@ -4,17 +4,36 @@ import { response } from 'express';
 import * as L from 'leaflet';
 import { HttpClient } from '@angular/common/http';
 import { url } from 'inspector';
+/*import {DBServices } from '../db.services';*/
 @Component({
   selector: 'app-stade',
   templateUrl: './stade.component.html',
   styleUrls: ['./stade.component.css']
 })
-export class StadeComponent {
- 
+export class StadeComponent implements OnInit{
   zoom: number = 8;
   latitude= 33.886917;
   longitude=9.537499;
+
+   villes: any =[];
+  ngOnInit(): void 
+  {
+    this.http.get(this.url).subscribe(response=>{
+      this.villes= response;
+    }
+      )
+  }
+  constructor(private http:HttpClient){}
+    url= "http://192.168.1.45:8080/villes";
   
+
+  
+
+
+  /*villes;
+  constructor(dbservices:DBServices){
+this.villes=dbservices.getall();
+  }  */
 
 }
 
